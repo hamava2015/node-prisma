5dc1b3fb6d220809d9180997f90b296fcc817f31
Afshin <fallahi_a@yahoo.com>
2023-10-24 14:07:24 -0400

    refactor equipment

---
+++ src/controllers/equipmentController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { addEquipment, getEquipments, checkoutEquipment } from '../services/equipmentService';
+import { IEquipmentRequestBody } from '../interfaces/equipment';
+
+export const addEquipmentController = async (req: any, res: Response) => {
+    try {
+        const data = req.body as IEquipmentRequestBody;
+        const equipment = await addEquipment(data, req.user);
+        res.status(201).json(equipment);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+};
+
+export const getEquipmentsController = async (req: any, res: Response) => {
+    try {
+        const { type, location } = req.query;
+        const equipmentList = await getEquipments(type, location);
+        res.json(equipmentList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to retrieve equipment list' });
+    }
+};
+
+export const checkoutEquipmentController = async (req: any, res: Response) => {
+    try {
+        const equipmentId = req.body.equipmentId;
+        const result = await checkoutEquipment(equipmentId, req.user);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to check out equipment' });
+    }
+};
