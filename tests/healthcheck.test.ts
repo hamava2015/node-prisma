36cea7b3dd32e0583de7c7686da4eb9a59eb514e
Afshin <fallahi_a@yahoo.com>
2023-10-24 15:34:59 -0400

    added some tests

--- tests/healthcheck.test.ts
+++ tests/healthcheck.test.ts
@@ -1,11 +1,11 @@
 import request from 'supertest';
-import app from '../src/server';
+import app from '../src';
 
 describe('Server Health Check', () => {
     it('should respond with status 200 and message "pong"', async () => {
         const response = await request(app).get('/healthcheck/ping');
 
         expect(response.status).toBe(200);
-        expect(response.body.message).toBe('pong');
+        return expect(response.body.message).toBe('pong');
     });
 });
