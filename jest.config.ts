c4cb00af6aa38ff5ed37d5e6f756c02837a241eb
Afshin <fallahi_a@yahoo.com>
2023-10-24 12:43:50 -0400

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json',
+        },
+    },
+};
